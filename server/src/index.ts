da5e5779c6dba21bc8bfb58121cbab216697485c
gkorompis <gracia.korompis@alumni.i3l.ac.id>
2025-12-03 15:31:07 +0700

    chores: initiating express connection and default middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("init server");
+import * as dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+
+// declare secrets env
+dotenv.config();
+const PORT = process.env.PORT || 5002
+
+// initiating express
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes
+// app.use("/products", productsRoute)
+
+app.get("/", (req,res)=>{
+    res.send("testing backend server")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server running on PORT ${PORT}`)
+})
+
+
+
